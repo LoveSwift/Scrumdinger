f42800c3623f61d1cf86169844ee6d8fc09a628d
SURYAKANTSHARMA <suryakantsharma84@gmail.com>
2024-05-12 14:50:25 +0530

    Initial commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Surya on 12/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
